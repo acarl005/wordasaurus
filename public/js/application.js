cfcdc5903aa4a2358c8c92e7e773dc74edc730c8
Andrew Carlson <acarl005@g.ucla.edu>
2015-05-13 17:23:03 -0700

    multiple wors are insertable, but not as easilt reversed

--- public/js/application.js
+++ public/js/application.js
@@ -26,15 +26,16 @@ angular.module('wordasaurus', ['ngRoute'])
   var vm = this;
   var endpoint = 'http://words.bighugelabs.com/api/2/36312c87d8575d1476ece69c8dd8bdc1/'
   User.current().success(function(res) {
-    $scope.user = res
+    $scope.user = res;
     Piece.all($scope.user.id).success(function(res) {$scope.pieces = res});
   });
   $scope.setActivePiece = function(piece) {
-    vm.activePiece = piece
-    vm.syn_json = JSON.parse(JSON.parse(piece.syn_json));
+    vm.activePiece = piece;
+    vm.syn_json = JSON.parse(piece.syn_json);
   }
   $scope.getSyns = function(word) {
     if (!vm.syn_json[word]) {
+      console.log('getting');
       $http.get(endpoint+word+'/json').success(function(res) {
         vm.syn_json[word] = res
         $scope.saveSyns();
@@ -44,11 +45,14 @@ angular.module('wordasaurus', ['ngRoute'])
   $scope.setActiveWord = function(index, word) {
     vm.activeWord = word.toLowerCase().match(/[a-z]+/g)[0];
     vm.activeIndex = index;
-    $scope.getSyns(vm.activeWord)
+    $scope.getSyns(vm.activeWord);
   };
   $scope.embedWord = function(word) {
-    var target = $('#show-piece span:nth-child(' + (vm.activeIndex+1) + ')')
-    target.text(target.text().replace(/[a-z]+/i, word));
+    // var target = $('#show-piece span:nth-child(' + (vm.activeIndex+1) + ')')
+    // target.text(target.text().replace(/[a-z\s]+/i, word));
+    var pieces = vm.activePiece.content.split(' ');
+    pieces[vm.activeIndex] = pieces[vm.activeIndex].replace(/[a-z]+/i, word);
+    vm.activePiece.content = pieces.join(' ');
   };
   $scope.saveSyns = function() {
     $http.post('/pieces/'+vm.activePiece.id+'/syns', {syn_json: JSON.stringify(vm.syn_json)} )
@@ -64,6 +68,7 @@ angular.module('wordasaurus', ['ngRoute'])
   $scope.refreshActiveFromSyn = function() {
     vm.activePiece.content = $scope.stripSpan();
     $('#edit-piece').val(vm.activePiece.content);
+    debugger;
   }
   $scope.refreshActiveFromEdit = function() {
     vm.activePiece.content = $('#edit-piece').val();
@@ -80,7 +85,7 @@ angular.module('wordasaurus', ['ngRoute'])
 .factory('Piece', ['$http', function NoteFactory($http) {
   return {
     all: function(user_id) {
-      return $http.get('users/'+user_id+'/pieces')
+      return $http.get('users/'+user_id+'/pieces');
     }
   };
 }])
