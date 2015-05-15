07ae54001df3e89dab7396253766384ecefbd464
Andrew Carlson <acarl005@g.ucla.edu>
2015-05-15 15:33:14 -0700

    edit and delete tests:

--- spec/features/piece_spec.rb
+++ spec/features/piece_spec.rb
@@ -19,7 +19,7 @@ describe 'piece CRUD', js: true, type: :feature do
     click_link('My Pieces')
   end
 
-  it 'should create a piece' do
+  it 'should create and display piece' do
     click_button('Add Piece')
     fill_in('title', with: 'test')
     fill_in('content', with: 'a bunch of test text')
@@ -27,6 +27,31 @@ describe 'piece CRUD', js: true, type: :feature do
     click_button('My Pieces')
     page.find('.dropdown li:first-of-type').click
     page.should have_content('a bunch of test text')
+  end
+
+  context 'with pieces' do
+    before do
+      click_button('Add Piece')
+      fill_in('title', with: 'test')
+      fill_in('content', with: 'a bunch of test text')
+      click_button('Submit')
+      click_button('My Pieces')
+      page.find('.dropdown li:first-of-type').click
+      page.should have_content('a bunch of test text')
+    end
+
+    it 'should delete' do
+      click_button('Delete')
+      page.should_not have_content('a bunch of test text')
+      Piece.count.should eq(0)
+    end
+
+    it 'should edit the text' do
+      click_button('Edit')
+      fill_in('content', with: 'reogherougbeprougnre')
+      click_button('Done')
+      page.should have_content('reogherougbeprougnre')
+    end
   end
 
 end
