032748e2c4ebd24c36f8090f5485f524f495dd6e
acarl005 <acarl005@g.ucla.edu>
2015-05-16 22:12:39 -0700

    use scope instead of this and fix rspec

--- public/js/application.js
+++ public/js/application.js
@@ -18,7 +18,7 @@ angular.module('wordasaurus', ['ngRoute'])
 }])
 
 .controller('HomeController', ['$scope', '$http', 'User' , 'Piece', function ($scope, $http, User, Piece) {
-  var vm = this;
+  // var vm = this;
   User.current().success(function(res) {
     $scope.user = res;
     Piece.all($scope.user.id).success(function(res) {$scope.pieces = res});
@@ -29,41 +29,41 @@ angular.module('wordasaurus', ['ngRoute'])
       content: content,
     }).success(function(payload) {
       $scope.pieces.push(payload);
-      vm.tab = 'button';
+      $scope.tab = 'button';
       $('#new-title').val('');
       $('#new-piece').val('');
     });
   };
   $scope.setActivePiece = function(piece) {
-    vm.activePiece = piece;
-    vm.syn_json = JSON.parse(piece.syn_json);
+    $scope.activePiece = piece;
+    $scope.syn_json = JSON.parse(piece.syn_json);
   };
   $scope.getSyns = function(word) {
-    if (!vm.syn_json[word]) {
+    if (!$scope.syn_json[word]) {
       console.log('getting');
       $http.get('/synonyms/'+word).success(function(res) {
-        vm.syn_json[word] = res
+        $scope.syn_json[word] = res
         $scope.saveSyns();
       });
     }
   };
   $scope.setActiveWord = function(index, word) {
-    vm.activeWord = word.toLowerCase().match(/[a-z]+/g)[0];
-    vm.activeIndex = index;
-    $scope.getSyns(vm.activeWord);
+    $scope.activeWord = word.toLowerCase().match(/[a-z]+/g)[0];
+    $scope.activeIndex = index;
+    $scope.getSyns($scope.activeWord);
   };
   $scope.embedWord = function(word) {
-    // var target = $('#show-piece span:nth-child(' + (vm.activeIndex+1) + ')')
+    // var target = $('#show-piece span:nth-child(' + ($scope.activeIndex+1) + ')')
     // target.text(target.text().replace(/[a-z\s]+/i, word));
-    var pieces = vm.activePiece.content.split(' ');
-    pieces[vm.activeIndex] = pieces[vm.activeIndex].replace(/[a-z]+/i, word);
-    vm.activePiece.content = pieces.join(' ');
+    var pieces = $scope.activePiece.content.split(' ');
+    pieces[$scope.activeIndex] = pieces[$scope.activeIndex].replace(/[a-z]+/i, word);
+    $scope.activePiece.content = pieces.join(' ');
   };
   $scope.saveSyns = function() {
-    $http.post('/pieces/'+vm.activePiece.id+'/syns', {syn_json: JSON.stringify(vm.syn_json)} )
+    $http.post('/pieces/'+$scope.activePiece.id+'/syns', {syn_json: JSON.stringify($scope.syn_json)} )
   };
   $scope.savePiece = function() {
-    $http.put('/pieces/'+vm.activePiece.id, {content: $scope.stripSpan()} ).success(function() {
+    $http.put('/pieces/'+$scope.activePiece.id, {content: $scope.stripSpan()} ).success(function() {
       alert('Saved!');
     });
   };
@@ -71,17 +71,17 @@ angular.module('wordasaurus', ['ngRoute'])
     return ($('#show-piece').text().match(/\S+/gi) || []).join(' ')
   };
   $scope.refreshActiveFromSyn = function() {
-    vm.activePiece.content = $scope.stripSpan();
-    $('#edit-piece').val(vm.activePiece.content);
+    $scope.activePiece.content = $scope.stripSpan();
+    $('#edit-piece').val($scope.activePiece.content);
   };
   $scope.refreshActiveFromEdit = function() {
-    vm.activePiece.content = $('#edit-piece').val();
+    $scope.activePiece.content = $('#edit-piece').val();
   };
   $scope.deleteActive = function() {
-    $http.delete('/pieces/'+vm.activePiece.id).success(function(id) {
-      vm.syn_json = {};
-      vm.activePiece = {};
-      vm.activeWord = '';
+    $http.delete('/pieces/'+$scope.activePiece.id).success(function(id) {
+      $scope.syn_json = {};
+      $scope.activePiece = {};
+      $scope.activeWord = '';
       for (var i = 0; i < $scope.pieces.length; i++) {
         if ($scope.pieces[i].id == id) {
           $scope.pieces.splice(i,1);
@@ -89,9 +89,9 @@ angular.module('wordasaurus', ['ngRoute'])
       };
     });
   };
-  vm.syn_json = {};
-  vm.activePiece = {};
-  vm.activeWord = '';
+  $scope.syn_json = {};
+  $scope.activePiece = {};
+  $scope.activeWord = '';
 }])
 
 .factory('Piece', ['$http', function NoteFactory($http) {
