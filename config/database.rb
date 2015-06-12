066739e18c5314b1939efd262505962a4ca77fec
Andrew Carlson <acarl005@g.ucla.edu>
2015-06-12 15:49:02 -0700

    set db password for postgres

--- config/database.rb
+++ config/database.rb
@@ -36,7 +36,7 @@ ActiveRecord::Base.establish_connection(
   :host     => db.host,
   :port     => db.port,
   :username => db.user,
-  :password => db.password,
+  :password => db.password || ENV['DB_PASSWORD'],
   :database => DB_NAME,
   :encoding => 'utf8'
 )
